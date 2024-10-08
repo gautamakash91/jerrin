eaba3b714e1dfad8069563585ed2a0548939882b
gautamakash91 <gautamakash91@gmail.com>
2024-10-08 09:24:16 +0530

    ready for hw 1

---
+++ src/components/counter.jsx
@@ -0,0 +1,35 @@
+function Counter(){
+  return (
+    <div>
+      Hello world
+      <Addition />
+    </div>
+  )
+}
+
+export function Calculator(){
+  return (
+    <div>
+      this is a Calculator
+      <Addition />
+      <Counter />
+    </div>
+  )
+}
+
+export function Addition(){
+  return (
+    <div>
+      this is the addition component
+    </div>
+  )
+}
+
+export default Counter; 
+
+//RULES
+// 1. Component name should always start with a capital letter 
+// 2. A component needs to be exported to be imported in any other file. 
+// 3. Only one component can be exported by default. Other components in the same file can be exported normally. 
+// 4. Normally exported components have to be put inside { } when they are imported. 
+// 5. 
