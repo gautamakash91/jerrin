f90175ce450e128d6afb3ceef3b1666050b203f9
gautamakash91 <gautamakash91@gmail.com>
2024-10-10 09:26:06 +0530

    session 4: state variables

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,35 +1,37 @@
-function Counter(){
+import { useState } from "react";
+
+function Counter() {
+  const [a, setA] = useState(10);
+
+  function IncreaseCount() {
+    setA(a + 1);
+  }
+
+  function handleChange(event){
+    // console.log(event);
+    var b = parseInt(event.target.value)
+    setA(b);
+  }
+
   return (
     <div>
-      Hello world
-      <Addition />
+      <input 
+        value={a}
+        onChange={handleChange}
+      />
+      Count is {a}
+      <button onClick={IncreaseCount}>
+        increase count
+      </button>
     </div>
-  )
+  );
 }
 
-export function Calculator(){
-  return (
-    <div>
-      this is a Calculator
-      <Addition />
-      <Counter />
-    </div>
-  )
-}
-
-export function Addition(){
-  return (
-    <div>
-      this is the addition component
-    </div>
-  )
-}
-
-export default Counter; 
+export default Counter;
 
 //RULES
-// 1. Component name should always start with a capital letter 
-// 2. A component needs to be exported to be imported in any other file. 
-// 3. Only one component can be exported by default. Other components in the same file can be exported normally. 
-// 4. Normally exported components have to be put inside { } when they are imported. 
+// 1. Component name should always start with a capital letter
+// 2. A component needs to be exported to be imported in any other file.
+// 3. Only one component can be exported by default. Other components in the same file can be exported normally.
+// 4. Normally exported components have to be put inside { } when they are imported.
 // 5. inside of the render function there always has to be a parent div
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Calculator from "./homework/calculator/calculator.jsx";
+// import Calculator from "./homework/calculator/calculator.jsx";
+import Counter from "./components/counter.jsx";
 // import Counter, { Calculator } from "./components/counter.jsx"; //relative path
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Calculator />
+    <Counter />
   </StrictMode>
 );
