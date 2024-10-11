c114d59a9a8e1c748e47e049752ad1a45040f347
gautamakash91 <gautamakash91@gmail.com>
2024-10-11 10:39:04 +0530

    session 5: styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+// import "./index.css";
 // import Calculator from "./homework/calculator/calculator.jsx";
 import Counter from "./components/counter.jsx";
+import Styling from "./components/styling.jsx";
 // import Counter, { Calculator } from "./components/counter.jsx"; //relative path
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Counter />
+    <Styling />
   </StrictMode>
 );
